35c1e6079ec28452acea6239b825ed932f6ffe99
Pedro Fontes <pvfontes1000@gmail.com>
2020-04-06 16:30:21 -0300

    led init

--- OLED-Xplained-Pro-SPI/src/main.c
+++ OLED-Xplained-Pro-SPI/src/main.c
@@ -4,11 +4,66 @@
 #include "gfx_mono_text.h"
 #include "sysfont.h"
 
+// LED
+#define LED_PIO       PIOC
+#define LED_PIO_ID    ID_PIOC
+#define LED_IDX       8
+#define LED_IDX_MASK  (1 << LED_IDX)
+
+// Botão
+#define BUT_PIO       PIOA
+#define BUT_PIO_ID    ID_PIOA
+#define BUT_IDX       11
+#define BUT_IDX_MASK  (1 << BUT_IDX)
+
+// Botão 1
+#define BUT1_PIO            PIOD
+#define BUT1_PIO_ID         16
+#define BUT1_PIO_IDX        28
+#define BUT1_PIO_IDX_MASK   (1u << BUT1_PIO_IDX)
+
+// LED1 oled
+#define LED1_PIO      PIOA
+#define LED1_PIO_ID   ID_PIOA
+#define LED1_IDX      0
+#define LED1_IDX_MASK (1 << LED1_IDX)
+
+// LED2 oled
+#define LED2_PIO      PIOC
+#define LED2_PIO_ID   ID_PIOC
+#define LED2_IDX      30
+#define LED2_IDX_MASK (1 << LED2_IDX)
+
+// LED3 oled
+#define LED3_PIO      PIOB
+#define LED3_PIO_ID   ID_PIOB
+#define LED3_IDX      2
+#define LED3_IDX_MASK (1 << LED3_IDX)
+
+int init(){
+		
+	pmc_enable_periph_clk(LED_PIO_ID);
+	pio_configure(LED_PIO, PIO_OUTPUT_0, LED_IDX_MASK, PIO_DEFAULT);
+	
+	pmc_enable_periph_clk(LED1_PIO_ID);
+	pio_configure(LED1_PIO, PIO_OUTPUT_1, LED1_IDX_MASK, PIO_DEFAULT);
+
+	pmc_enable_periph_clk(LED2_PIO_ID);
+	pio_configure(LED2_PIO, PIO_OUTPUT_1, LED2_IDX_MASK, PIO_DEFAULT);
+
+	pmc_enable_periph_clk(LED3_PIO_ID);
+	pio_configure(LED3_PIO, PIO_OUTPUT_1, LED3_IDX_MASK, PIO_DEFAULT);
+}
+
 int main (void)
 {
+	// // Disativa WatchDog Timer
+	WDT->WDT_MR = WDT_MR_WDDIS;
+	
 	board_init();
 	sysclk_init();
 	delay_init();
+	init();
 
   // Init OLED
 	gfx_mono_ssd1306_init();
@@ -19,6 +74,7 @@ int main (void)
 
   /* Insert application code here, after the board has been initialized. */
 	while(1) {
+		
 
 	}
 }
